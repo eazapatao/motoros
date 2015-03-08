8a2a0e4e4314add3f05ca0df784dc471f2e0848b
Luis Eduardo <leescobarg@gmail.com>
2015-03-08 17:59:29 -0500

    fi datatables issue

--- asset/js/scripts.js
+++ asset/js/scripts.js
@@ -3,10 +3,7 @@
  */
 var base_url = $("#base_url").val();
 
-$('#client_list, #line_list, #banco_list' +
-'#usuario_list, nomina_list, #prestamo_list, #operacion_list').DataTable({
-
-});
+$('#client_list, #line_list, #banco_list, #usuario_list, #nomina_list, #prestamo_list, #operacion_list').DataTable();
 
 $("#guardar_alquiler").click(function(){
     $.ajax({
@@ -20,44 +17,6 @@ $("#guardar_alquiler").click(function(){
 
     });
 });
-
-<<<<<<< HEAD
-=======
-$("#guardar_detallebanco").click(function(){
-    $.ajax({
-        type: "POST",
-        url: base_url+"index.php/detallebanco/guardar_detallebanco/",
-        data: $("#form_detallebanco").serialize()
-    })
-});
-
-$("#guardar_historial").click(function(){
-    $.ajax({
-        type: "POST",
-        url: base_url+"index.php/linea/guardar_historial/",
-        data: $("#form_historial").serialize()
-    })
-});
-
-
-$("#guardar_prestamoempleado").click(function(){
-    $.ajax({
-        type: "POST",
-        url: base_url+"index.php/nomina/guardar_prestamo/",
-        data: $("#form_prestamoempleado").serialize()
-    })
-});
-
-$("#guardar_nomina").click(function(){
-    $.ajax({
-        type: "POST",
-        url: base_url+"index.php/nomina/guardar_nomina/",
-        data: $("#form_nomina").serialize()
-    })
-});
-
-
->>>>>>> ae713a83249b52dca9bed5a0a43fd3d97ed4d62b
 
 $('[data-toggle=confirmation]').confirmation({
     title: '¿Esta seguro de eliminar este registro?',
