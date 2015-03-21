91116c8b5af0bac58879503dea44a1d173b37878
eazapatao <andres.asi25@gmail.com>
2015-03-21 11:24:46 -0500

    prueba

--- application/views/templates/admin_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->load->view("templates/head");
-$this->load->view("templates/header");
-$this->load->view("templates/admin_sidebar");
-$this->load->view($main_content);
-$this->load->view("templates/footer");
-
--- application/views/templates/user_template.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$this->load->view("templates/head");
-$this->load->view("templates/header");
-$this->load->view("templates/user_sidebar");
-$this->load->view($main_content);
-$this->load->view("templates/footer");
-
