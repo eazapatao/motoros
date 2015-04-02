4d72855fdbd7aabd48b1aa263f7ebf85cb6ad11d
Andres <andres.asi25@gmail.com>
2015-04-02 09:33:57 -0500

    nombre de usuario en la parte superior

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -16,6 +16,8 @@
 <div class="navbar-right">
 <ul class="nav navbar-nav">
 
+
+
 <!-- Messages: style can be found in dropdown.less-->
 <li class="dropdown notifications-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -53,7 +55,7 @@
 <li class="dropdown user user-menu">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         <i class="glyphicon glyphicon-user"></i>
-        <span>Jane Doe <i class="caret"></i></span>
+        <span>NOMBRE DEL USUARIO<i class="caret"></i></span>
     </a>
     <ul class="dropdown-menu">
         <!-- User image -->
